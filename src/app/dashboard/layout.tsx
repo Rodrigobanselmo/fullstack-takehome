5aa9b0950f274b03327cddb30052a3de44e69cf7
Rodrigo Anselmo <rodrigobanselmo@gmail.com>
2025-06-24 18:11:18 -0300

    feat: update dashboard layout with logo and styling adjustments

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import LogoutButton from "~/components/ui/logout-button/logout-button";
 import { getUserFromCookie } from "~/lib/auth";
 import styles from "./layout.module.css";
 import { UserProvider } from "~/context/user-context";
+import Image from "next/image";
 
 export default async function DashboardLayout({
   children,
@@ -22,12 +23,28 @@ export default async function DashboardLayout({
         <header className={styles.header}>
           <div className={styles.headerContainer}>
             <div className={styles.headerContent}>
-              <h1 className={styles.title}>Dashboard</h1>
-              <p
-                className={`${styles.subtitle} ${styles.subtitleMobileHidden}`}
-              >
-                Welcome, {user.username} ({user.role.toLowerCase()})
-              </p>
+              <span className={styles.logo} aria-label="Home" title="Dashboard">
+                <Image
+                  src="/favicon.ico"
+                  alt="Dashboard Icon"
+                  width={28}
+                  height={28}
+                  style={{ display: "block" }}
+                  priority
+                />
+              </span>
+              <div>
+                <h1
+                  className={`${styles.title} ${styles.subtitleMobileHidden}`}
+                >
+                  Dashboard
+                </h1>
+                <p
+                  className={`${styles.subtitle} ${styles.subtitleMobileHidden}`}
+                >
+                  Welcome, {user.username} ({user.role.toLowerCase()})
+                </p>
+              </div>
             </div>
             <div style={{ display: "flex", alignItems: "center", gap: 24 }}>
               <NavbarLayout />
